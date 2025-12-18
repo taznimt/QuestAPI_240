21254572d5f90268f7282a9437591fda6f4d905f
taznimt <taznimanwar06@gmail.com>
2025-12-18 18:35:41 +0700

    feat: implement PenyediaViewModel Factory for dependency injection

---
+++ app/src/main/java/com/example/belajar/viewmodel/provider/PenyeidaViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.belajar.viewmodel.provider
+
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.belajar.repositori.AplikasiDataSiswa
+import com.example.belajar.viewmodel.EntryViewModel
+import com.example.belajar.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiDataSiswa
+        )
+object PenyeidaViewModel {
+    val Factory =viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+    }
+}
