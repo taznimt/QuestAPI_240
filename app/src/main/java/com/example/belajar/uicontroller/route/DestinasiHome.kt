7dd571b16a30ee41679a800c26e3e8401ffad37e
taznimt <taznimanwar06@gmail.com>
2025-12-18 16:55:19 +0700

    add DestinasiHome object for navigation

---
+++ app/src/main/java/com/example/belajar/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.belajar.uicontroller.route
+
+import com.example.belajar.R
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val tittleRes = R.string.app_name
+}
