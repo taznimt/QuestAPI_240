ac07bb1fdb749e12dca9c1bd15ea566cd5cfbe99
taznimt <taznimanwar06@gmail.com>
2025-12-18 17:31:23 +0700

    implement EntrySiswaScreen Composable

---
+++ app/src/main/java/com/example/belajar/view/HalamanEntry.kt
@@ -0,0 +1,17 @@
+package com.example.belajar.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.belajar.viewmodel.EntryViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
