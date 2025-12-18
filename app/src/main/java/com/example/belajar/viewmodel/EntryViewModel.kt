cccddbba071581a779c6b59dc6d75544c748cc3e
taznimt <taznimanwar06@gmail.com>
2025-12-18 17:10:40 +0700

    add input validation logic in EntryViewModel

--- app/src/main/java/com/example/belajar/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/belajar/viewmodel/EntryViewModel.kt
@@ -1,12 +1,23 @@
 package com.example.belajar.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.belajar.modeldata.DetailSiswa
 import com.example.belajar.modeldata.UIStateSiswa
 import com.example.belajar.repositori.RepositoryDataSiswa
 
 class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
-        viewModel() {
-        var uiStateSiswa by mutableStateOf(UIStateSiswa())
-            private set
+    ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):
+       Boolean {
+       return with(uiState) {
+           nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+       }
+    }
 
-        }
+
+    }
