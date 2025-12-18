30fe5db9f93fe2aaa8dd4748697301edde9cd31c
taznimt <taznimanwar06@gmail.com>
2025-12-18 18:26:54 +0700

    feat: implement HomeViewModel and StatusUiSiswa for data state handling

---
+++ app/src/main/java/com/example/belajar/viewmodel/HomeViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.belajar.viewmodel
+
+import android.net.http.HttpException
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.belajar.modeldata.DataSiswa
+import com.example.belajar.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init{
+        loadSiswa()
+    }
+
+    fun loadSiswa(){
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            }catch (e: IOException){
+                StatusUiSiswa.Error
+            }
+            catch (e: HttpException){
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
