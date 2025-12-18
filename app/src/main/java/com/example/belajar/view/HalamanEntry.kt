86d182c4cf69f8cdf1baf28ff1c46a640e9a39c7
taznimt <taznimanwar06@gmail.com>
2025-12-18 17:33:31 +0700

    implement Scaffold and TopAppBar with scroll behavior

--- app/src/main/java/com/example/belajar/view/HalamanEntry.kt
+++ app/src/main/java/com/example/belajar/view/HalamanEntry.kt
@@ -1,8 +1,12 @@
 package com.example.belajar.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.belajar.viewmodel.EntryViewModel
 
@@ -13,5 +17,16 @@ fun EntrySiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
-
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.tittleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        })
 }
