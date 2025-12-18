d2e7b9a1efd8462f0b736edd5a636bef62dbcaf1
taznimt <taznimanwar06@gmail.com>
2025-12-18 18:40:35 +0700

    feat: implement NavHost and navigation flow for the app

---
+++ app/src/main/java/com/example/belajar/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,40 @@
+package com.example.belajar.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.belajar.uicontroller.route.DestinasiEntry
+import com.example.belajar.uicontroller.route.DestinasiHome
+import com.example.belajar.view.EntrySiswaScreen
+import com.example.belajar.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier
+){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate
+                (DestinasiEntry.route) },
+                navigateToItemUpdate = {
+//                  navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome
+                .route) })
+        }
+    }
+}
