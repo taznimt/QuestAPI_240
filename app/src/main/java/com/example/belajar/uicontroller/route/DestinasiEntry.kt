19e6bab2c3eb71ee4aac67261d31858941777ccb
taznimt <taznimanwar06@gmail.com>
2025-12-18 16:42:05 +0700

    define navigation route for Siswa Entry screen

---
+++ app/src/main/java/com/example/belajar/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.belajar.uicontroller.route
+
+import com.example.belajar.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val tittleRes = R.string.entry_siswa
+}
