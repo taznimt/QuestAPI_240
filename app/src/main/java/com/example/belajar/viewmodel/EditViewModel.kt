f0b06baa33d50e0447dc14eb7292fbd79b579564
taznimt <taznimanwar06@gmail.com>
2025-12-25 16:23:17 +0700

    feat: add uiStateSiswa to manage student data state

--- app/src/main/java/com/example/belajar/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/belajar/viewmodel/EditViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.belajar.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.belajar.modeldata.UIStateSiswa
 import com.example.belajar.repositori.RepositoryDataSiswa
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
 RepositoryDataSiswa
 ): ViewModel() {
-
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 }
