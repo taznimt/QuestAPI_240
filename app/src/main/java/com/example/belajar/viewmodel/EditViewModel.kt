6050b3c41c201e23b8ecc09a127dcebb2643b875
taznimt <taznimanwar06@gmail.com>
2025-12-25 16:21:09 +0700

    feat: add EditViewModel with SavedStateHandle and Repository injection

---
+++ app/src/main/java/com/example/belajar/viewmodel/EditViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.belajar.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.belajar.repositori.RepositoryDataSiswa
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa
+): ViewModel() {
+
+}
