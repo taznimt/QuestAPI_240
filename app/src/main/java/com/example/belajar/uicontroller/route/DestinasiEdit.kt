3f10a235a07530270d6905619a8ab2f43234ded6
taznimt <taznimanwar06@gmail.com>
2025-12-25 17:25:41 +0700

    feat: add navigation route for EditSiswa

--- app/src/main/java/com/example/belajar/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/belajar/uicontroller/route/DestinasiEdit.kt
@@ -4,6 +4,7 @@ import com.example.belajar.R
 
 object DestinasiEdit : DestinasiNavigasi {
     override val route = "item_edit"
+
     override val titleRes = R.string.edit_siswa
     const val itemIdArg = "itemId"
     val routeWithArgs = "$route/{$itemIdArg}"
