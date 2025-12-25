1c191fac553e05eaa67b9abc57d2ab8c9500d76f
taznimt <taznimanwar06@gmail.com>
2025-12-25 16:00:45 +0700

    implement getSatuSiswa delegation to service

--- app/src/main/java/com/example/belajar/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/belajar/repositori/RepositoryDataSiswa.kt
@@ -12,7 +12,6 @@ interface RepositoryDataSiswa {
     suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
 
     suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
-
 }
 
 class JaringanRepositoryDataSiswa(
@@ -20,4 +19,5 @@ class JaringanRepositoryDataSiswa(
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa : DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 }
