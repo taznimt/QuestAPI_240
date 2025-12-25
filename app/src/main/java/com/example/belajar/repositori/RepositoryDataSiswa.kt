d1a11c9fdb25ff1ae9228199434cccb7b3367233
taznimt <taznimanwar06@gmail.com>
2025-12-25 15:59:13 +0700

    add hapusSatuSiswa method

--- app/src/main/java/com/example/belajar/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/belajar/repositori/RepositoryDataSiswa.kt
@@ -11,6 +11,8 @@ interface RepositoryDataSiswa {
 
     suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
 
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
+
 }
 
 class JaringanRepositoryDataSiswa(
