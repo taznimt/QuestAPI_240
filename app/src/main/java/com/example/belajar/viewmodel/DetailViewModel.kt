b20a8736806878c1c2505ef787a056bfa8531b1b
taznimt <taznimanwar06@gmail.com>
2025-12-25 16:38:51 +0700

    feat: implement fetch student detail with error handling

--- app/src/main/java/com/example/belajar/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/belajar/viewmodel/DetailViewModel.kt
@@ -5,8 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.belajar.modeldata.DataSiswa
 import com.example.belajar.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
@@ -25,5 +29,17 @@ RepositoryDataSiswa
 
     init {
         getSatuSiswa()
+    }
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: HttpException) {
+                StatusUIDetail.Error
+            }
+        }
     }
 }
