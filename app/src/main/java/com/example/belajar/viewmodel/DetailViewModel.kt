980dd3f4ddfcbe8837980479d7ac3bbd8a8fd821
taznimt <taznimanwar06@gmail.com>
2025-12-25 16:35:10 +0700

    feat: add DetailViewModel for student detail logic

--- app/src/main/java/com/example/belajar/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/belajar/viewmodel/DetailViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.belajar.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.belajar.modeldata.DataSiswa
+import com.example.belajar.repositori.RepositoryDataSiswa
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa
+): ViewModel() {
+
 }
