45b95bc55163076c4b393c576f9df10f6ac61a6f
taznimt <taznimanwar06@gmail.com>
2025-12-25 16:39:50 +0700

    feat: implement delete student functionality in DetailViewModel

--- app/src/main/java/com/example/belajar/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/belajar/viewmodel/DetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.belajar.viewmodel
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -11,6 +12,7 @@ import com.example.belajar.repositori.RepositoryDataSiswa
 import kotlinx.coroutines.launch
 import okio.IOException
 import retrofit2.HttpException
+import retrofit2.Response
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
@@ -40,6 +42,16 @@ RepositoryDataSiswa
             } catch (e: HttpException) {
                 StatusUIDetail.Error
             }
+        }
+    }
+    @SuppressLint("SuspiciousIndentation")
+    suspend fun hapusSatuSiswa() {
+        val resp: Response<Void> = repositoryDataSiswa.hapusSatuSiswa(idSiswa)
+
+        if (resp.isSuccessful){
+            println("Sukses Hapus Data : ${resp.message()}")
+        }else{
+            println("Gagal Hapus Data : ${resp.errorBody()}")
         }
     }
 }
