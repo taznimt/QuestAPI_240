4a0be8f551c72fc3f7b379b309d6078c526ab286
taznimt <taznimanwar06@gmail.com>
2025-12-25 16:28:44 +0700

    feat: implement student data update execution

--- app/src/main/java/com/example/belajar/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/belajar/viewmodel/EditViewModel.kt
@@ -8,9 +8,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.belajar.modeldata.DetailSiswa
 import com.example.belajar.modeldata.UIStateSiswa
+import com.example.belajar.modeldata.toDataSiswa
 import com.example.belajar.modeldata.toUiStateSiswa
 import com.example.belajar.repositori.RepositoryDataSiswa
 import kotlinx.coroutines.launch
+import retrofit2.Response
+
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
 RepositoryDataSiswa
@@ -31,6 +34,18 @@ RepositoryDataSiswa
     private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa,uiStateSiswa
+                .detailSiswa.toDataSiswa())
+
+            if (call.isSuccessful){
+                println("Update Sukses : ${call.message()}")
+            }else{
+                println("Update Error : ${call.errorBody()}")
+            }
         }
     }
 }
