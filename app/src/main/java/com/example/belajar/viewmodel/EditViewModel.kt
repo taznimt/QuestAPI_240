ad944d6de27214cefadf341d4ec18ace6f44dc65
taznimt <taznimanwar06@gmail.com>
2025-12-25 16:24:31 +0700

    feat: fetch initial student data in EditViewModel init

--- app/src/main/java/com/example/belajar/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/belajar/viewmodel/EditViewModel.kt
@@ -5,12 +5,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.belajar.modeldata.UIStateSiswa
+import com.example.belajar.modeldata.toUiStateSiswa
 import com.example.belajar.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
 RepositoryDataSiswa
 ): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
 }
